9aaeab8528af9148ec2a0b982444d67475ded9e5
hudsonbay <manuelmenendezalfonso@gmail.com>
2021-02-13 23:47:26 -0500

    added server: true

--- config/prod.exs
+++ config/prod.exs
@@ -14,7 +14,8 @@ config :mvp_api, MvpApiWeb.Endpoint,
   url: [scheme: "https", host: "mvp-api-jc.herokuapp.com", port: 443],
   force_ssl: [rewrite_on: [:x_forwarded_proto]],
   cache_static_manifest: "priv/static/cache_manifest.json",
-  secret_key_base: Map.fetch!(System.get_env(), "SECRET_KEY_BASE")
+  secret_key_base: Map.fetch!(System.get_env(), "SECRET_KEY_BASE"),
+  server: true
 
 # Do not print debug messages in production
 config :logger, level: :info
