009a2aaf9b103e72a6dc5f2cc419e22767f222fa
hudsonbay <manuelmenendezalfonso@gmail.com>
2021-02-14 00:53:05 -0500

    Back to Heroku db

--- config/prod.exs
+++ config/prod.exs
@@ -23,7 +23,7 @@ config :logger, level: :info
 # Configure your database
 config :mvp_api, MvpApi.Repo,
   adapter: Ecto.Adapters.Postgres,
-  url: System.get_env("ELEPHANT_DB"),
+  url: System.get_env("DATABASE_URL"),
   pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10"),
   ssl: true
 
