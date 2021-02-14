992771c3befb1ccef5457d1503d292f8ba5efb94
hudsonbay <manuelmenendezalfonso@gmail.com>
2021-02-14 01:29:25 -0500

    configured d]for Render

--- lib/mvp_api/repo.ex
+++ lib/mvp_api/repo.ex
@@ -1,5 +1,10 @@
 defmodule MvpApi.Repo do
   use Ecto.Repo,
     otp_app: :mvp_api,
-    adapter: Ecto.Adapters.Postgres
+    adapter: Ecto.Adapters.Postgres,
+    pool_size: 10
+
+  def init(_type, config) do
+    {:ok, Keyword.put(config, :url, System.get_env("DATABASE_URL"))}
+  end
 end
