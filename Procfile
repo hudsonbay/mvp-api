6fee9ba92264add22afdf620f32acd45cd0e63db
hudsonbay <manuelmenendezalfonso@gmail.com>
2021-02-13 23:51:26 -0500

    Changed Procfile

--- Procfile
+++ Procfile
@@ -1,1 +1,2 @@
-web: MIX_ENV=prod mix ecto.migrate && mix phx.server
+web: MIX_ENV=prod mix phx.server
+release: POOL_SIZE=2 MIX_ENV=prod mix ecto.migrate
--- config/prod.exs
+++ config/prod.exs
@@ -14,8 +14,7 @@ config :mvp_api, MvpApiWeb.Endpoint,
   url: [scheme: "https", host: "mvp-api-jc.herokuapp.com", port: 443],
   force_ssl: [rewrite_on: [:x_forwarded_proto]],
   cache_static_manifest: "priv/static/cache_manifest.json",
-  secret_key_base: Map.fetch!(System.get_env(), "SECRET_KEY_BASE"),
-  server: true
+  secret_key_base: Map.fetch!(System.get_env(), "SECRET_KEY_BASE")
 
 # Do not print debug messages in production
 config :logger, level: :info
