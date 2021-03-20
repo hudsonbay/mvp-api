433696eba8381ae785767dda040b94b0e1936491
hudsonbay <manuelmenendezalfonso@gmail.com>
2021-03-20 19:20:40 -0400

    Made some validation

--- lib/mvp_api/providers/provider.ex
+++ lib/mvp_api/providers/provider.ex
@@ -2,12 +2,16 @@ defmodule MvpApi.Providers.Provider do
   use Ecto.Schema
   import Ecto.Changeset
 
+  alias MvpApi.Processes.Process
+
   @primary_key {:id, :binary_id, autogenerate: true}
   @foreign_key_type :binary_id
   schema "providers" do
-    field :distance, :string
+    field :distance_km, :integer
     field :location, :string
     field :name, :string
+
+    many_to_many :processes, Process, join_through: "process_providers", on_replace: :delete
 
     timestamps()
   end
@@ -15,7 +19,8 @@ defmodule MvpApi.Providers.Provider do
   @doc false
   def changeset(provider, attrs) do
     provider
-    |> cast(attrs, [:name, :location, :distance])
-    |> validate_required([:name, :location, :distance])
+    |> cast(attrs, [:name, :location, :distance_km])
+    |> validate_required([:name, :location, :distance_km])
+    |> unique_constraint([:name])
   end
 end
