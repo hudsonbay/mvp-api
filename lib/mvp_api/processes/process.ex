e42df563fcd1a15c14a8ae0f496493dcc31edb94
hudsonbay <manuelmenendezalfonso@gmail.com>
2021-03-20 19:20:07 -0400

    Added changeset toprocess module to edit providers

--- lib/mvp_api/processes/process.ex
+++ lib/mvp_api/processes/process.ex
@@ -2,11 +2,15 @@ defmodule MvpApi.Processes.Process do
   use Ecto.Schema
   import Ecto.Changeset
 
+  alias MvpApi.Providers.Provider
+
   @primary_key {:id, :binary_id, autogenerate: true}
   @foreign_key_type :binary_id
   schema "processes" do
     field :description, :string
     field :name, :string
+
+    many_to_many :providers, Provider, join_through: "process_providers", on_replace: :delete
 
     timestamps()
   end
@@ -16,5 +20,15 @@ defmodule MvpApi.Processes.Process do
     process
     |> cast(attrs, [:name, :description])
     |> validate_required([:name, :description])
+  end
+
+  @doc """
+  changeset to process module for editing providers (adding the put_assoc/4)
+  """
+  def changeset_update_providers(%MvpApi.Processes.Process{} = process, providers) do
+    process
+    |> cast(%{}, [:name, :description])
+    # associate providers to the process
+    |> put_assoc(:providers, providers)
   end
 end
