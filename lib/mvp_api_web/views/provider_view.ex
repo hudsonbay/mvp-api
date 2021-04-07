9f683ad7e1c129de22d516a2222ae0961b70fafb
hudsonbay <manuelmenendezalfonso@gmail.com>
2021-04-06 20:58:03 -0400

    Created new views for the process provider

--- lib/mvp_api_web/views/provider_view.ex
+++ lib/mvp_api_web/views/provider_view.ex
@@ -1,6 +1,7 @@
 defmodule MvpApiWeb.ProviderView do
   use MvpApiWeb, :view
   alias MvpApiWeb.ProviderView
+  alias MvpApi.Providers.Formulas
 
   def render("index.json", %{providers: providers}) do
     %{data: render_many(providers, ProviderView, "provider.json")}
@@ -16,6 +17,29 @@ defmodule MvpApiWeb.ProviderView do
       name: provider.name,
       distance_km: provider.distance_km,
       location: provider.location
+    }
+  end
+
+  def render("show_process_providers.json", %{providers_of_process: providers_of_process}) do
+    %{
+      data:
+        render_many(providers_of_process, ProviderView, "process_provider.json",
+          as: :process_provider
+        )
+    }
+  end
+
+  def render("process_provider.json", %{process_provider: process_provider}) do
+    %{
+      process_id: process_provider.process_id,
+      provider_id: process_provider.provider_id,
+      provider_name: process_provider.provider_name,
+      goods_type: process_provider.goods_type,
+      number_supplies_year: process_provider.number_supplies_year,
+      tons_by_supplies: process_provider.tons_by_supplies,
+      distance_km: process_provider.distance_km,
+      location: process_provider.location,
+      anual_transportation_volume: Formulas.calculate_anual_transportation_volume(process_provider.tons_by_supplies, process_provider.number_supplies_year, process_provider.distance_km)
     }
   end
 end
