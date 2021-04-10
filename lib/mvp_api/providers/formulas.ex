7de6707b02751c3d8cceb10f70ebe4db78d0585a
hudsonbay <manuelmenendezalfonso@gmail.com>
2021-04-10 17:14:36 -0400

    Calculated total anual transportation volume

--- lib/mvp_api/providers/formulas.ex
+++ lib/mvp_api/providers/formulas.ex
@@ -1,5 +1,20 @@
 defmodule MvpApi.Providers.Formulas do
+  alias Numbers, as: N
+
   def calculate_anual_transportation_volume(tons_by_supplies, number_supplies_year, distance_km) do
     tons_by_supplies * number_supplies_year * distance_km
+  end
+
+  def calculate_total_anual_transportation_volume(process_providers) do
+    process_providers
+    |> Enum.map(fn %{
+                     tons_by_supplies: tons_by_supplies,
+                     number_supplies_year: number_supplies_year,
+                     distance_km: distance_km
+                   } ->
+      calculate_anual_transportation_volume(tons_by_supplies, number_supplies_year, distance_km)
+      |> N.to_float()
+    end)
+    |> Enum.sum()
   end
 end
