ea19f1e3cb1becae4e45ffb41485ed0b9df97a6d
hudsonbay <manuelmenendezalfonso@gmail.com>
2021-04-14 17:44:08 -0400

    Added example fake data

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -9,3 +9,92 @@
 #
 # We recommend using the bang functions (`insert!`, `update!`
 # and so on) as they will fail if something goes wrong.
+alias MvpApi.Repo
+
+Faker.start()
+
+alias MvpApi.Client.{Product, Factor}
+alias MvpApi.Accounts
+alias MvpApi.Providers.Provider
+alias MvpApi.Processes.Process
+alias MvpApi.Providers.ProcessProvider
+
+# Products with factors
+for _ <- 0..10 do
+
+  product = Repo.insert!(%Product{
+    name: Faker.Commerce.En.product_name_product()
+  })
+
+  Repo.insert!(%Factor{
+    name: "Dureza",
+    value: Faker.random_between(1, 10),
+    weighing: Faker.random_between(1, 10),
+    product_id: product.id
+  })
+
+  Repo.insert!(%Factor{
+    name: "Humedad",
+    value: Faker.random_between(1, 10),
+    weighing: Faker.random_between(1, 10),
+    product_id: product.id
+  })
+
+  Repo.insert!(%Factor{
+    name: "Maleabilidad",
+    value: Faker.random_between(1, 10),
+    weighing: Faker.random_between(1, 10),
+    product_id: product.id
+  })
+
+  Repo.insert!(%Factor{
+    name: "Consistencia",
+    value: Faker.random_between(1, 10),
+    weighing: Faker.random_between(1, 10),
+    product_id: product.id
+  })
+
+end
+
+# Providers
+provider1 = Repo.insert!(%Provider{
+  name: "ACINOX",
+  location: "La Lisa",
+  distance_km: 25
+})
+
+provider2 = Repo.insert!(%Provider{
+  name: "Industria polígráfica",
+  location: "Cerro",
+  distance_km: 12
+})
+
+# Processes
+process = Repo.insert!(%Process{
+  name: "Produccion de dulces finos",
+  description: "Proceso para producir dulces en una fabrica"
+})
+
+# Providers on processes
+Repo.insert!(%ProcessProvider{
+  number_supplies_year: 280,
+  tons_by_supplies: 3.0,
+  goods_type: "Leche fluida",
+  transportation_mode: :automotor,
+  provider_id: provider1.id,
+  process_id: process.id
+})
+
+Repo.insert!(%ProcessProvider{
+  number_supplies_year: 25,
+  tons_by_supplies: 10.0,
+  goods_type: "Leche en polvo",
+  transportation_mode: :automotor,
+  provider_id: provider2.id,
+  process_id: process.id
+})
+
+# Users
+Accounts.register_user(%{"email" => "manuel@gmail.com", "password" => "123qweasdzxc"})
+Accounts.register_user(%{"email" => "daniel@gmail.com", "password" => "123qweasdzxc"})
+Accounts.register_user(%{"email" => "monteagudo@gmail.com", "password" => "123qweasdzxc"})
