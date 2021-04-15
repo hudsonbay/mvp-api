4c38e1a30dda9841a1a1cd36553b3616c65733f3
hudsonbay <manuelmenendezalfonso@gmail.com>
2021-04-14 22:46:34 -0400

    Refactored and added calculate_final_evaluation

--- lib/mvp_api/providers/formulas.ex
+++ lib/mvp_api/providers/formulas.ex
@@ -1,20 +1,28 @@
 defmodule MvpApi.Providers.Formulas do
   alias Numbers, as: N
 
-  def calculate_anual_transportation_volume(tons_by_supplies, number_supplies_year, distance_km) do
-    tons_by_supplies * number_supplies_year * distance_km
+  def calculate_anual_transportation_volume(process_provider) do
+    process_provider.tons_by_supplies * process_provider.number_supplies_year *
+      process_provider.distance_km
   end
 
   def calculate_total_anual_transportation_volume(process_providers) do
     process_providers
-    |> Enum.map(fn %{
-                     tons_by_supplies: tons_by_supplies,
-                     number_supplies_year: number_supplies_year,
-                     distance_km: distance_km
-                   } ->
-      calculate_anual_transportation_volume(tons_by_supplies, number_supplies_year, distance_km)
+    |> Enum.map(fn x ->
+      calculate_anual_transportation_volume(x)
       |> N.to_float()
     end)
     |> Enum.sum()
+  end
+
+  def calculate_final_evaluation(provider_evaluation) do
+    (provider_evaluation.punctuality * provider_evaluation.punctuality_weighing +
+       provider_evaluation.quality * provider_evaluation.quality_weighing +
+       provider_evaluation.credit * provider_evaluation.credit_weighing +
+       provider_evaluation.relations * provider_evaluation.relations_weighing +
+       provider_evaluation.quantity * provider_evaluation.quantity_weighing) /
+      (provider_evaluation.punctuality_weighing + provider_evaluation.quality_weighing +
+         provider_evaluation.credit_weighing + provider_evaluation.relations_weighing +
+         provider_evaluation.quantity_weighing)
   end
 end
