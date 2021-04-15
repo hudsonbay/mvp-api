7ef711560023f1e13323d638af05ae2d0950fcce
hudsonbay <manuelmenendezalfonso@gmail.com>
2021-04-15 00:21:03 -0400

    Added ProviderEvaluation seeds

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -18,13 +18,14 @@ alias MvpApi.Accounts
 alias MvpApi.Providers.Provider
 alias MvpApi.Processes.Process
 alias MvpApi.Providers.ProcessProvider
+alias MvpApi.Providers.ProviderEvaluation
 
 # Products with factors
 for _ <- 0..10 do
-
-  product = Repo.insert!(%Product{
-    name: Faker.Commerce.En.product_name_product()
-  })
+  product =
+    Repo.insert!(%Product{
+      name: Faker.Commerce.En.product_name_product()
+    })
 
   Repo.insert!(%Factor{
     name: "Dureza",
@@ -53,30 +54,32 @@ for _ <- 0..10 do
     weighing: Faker.random_between(1, 10),
     product_id: product.id
   })
-
 end
 
 # Providers
-provider1 = Repo.insert!(%Provider{
-  name: "ACINOX",
-  location: "La Lisa",
-  distance_km: 25
-})
+provider1 =
+  Repo.insert!(%Provider{
+    name: "ACINOX",
+    location: "La Lisa",
+    distance_km: 25
+  })
 
-provider2 = Repo.insert!(%Provider{
-  name: "Industria polígráfica",
-  location: "Cerro",
-  distance_km: 12
-})
+provider2 =
+  Repo.insert!(%Provider{
+    name: "Industria polígráfica",
+    location: "Cerro",
+    distance_km: 12
+  })
 
 # Processes
-process = Repo.insert!(%Process{
-  name: "Produccion de dulces finos",
-  description: "Proceso para producir dulces en una fabrica"
-})
+process =
+  Repo.insert!(%Process{
+    name: "Produccion de dulces finos",
+    description: "Proceso para producir dulces en una fabrica"
+  })
 
 # Providers on processes
-Repo.insert!(%ProcessProvider{
+process_provider1 = Repo.insert!(%ProcessProvider{
   number_supplies_year: 280,
   tons_by_supplies: 3.0,
   goods_type: "Leche fluida",
@@ -85,13 +88,42 @@ Repo.insert!(%ProcessProvider{
   process_id: process.id
 })
 
-Repo.insert!(%ProcessProvider{
+process_provider2 = Repo.insert!(%ProcessProvider{
   number_supplies_year: 25,
   tons_by_supplies: 10.0,
   goods_type: "Leche en polvo",
   transportation_mode: :automotor,
   provider_id: provider2.id,
   process_id: process.id
+})
+
+# Provider evaluations
+Repo.insert!(%ProviderEvaluation{
+  credit: 5,
+  credit_weighing: 10,
+  punctuality: 3,
+  punctuality_weighing: 30,
+  quality: 4,
+  quality_weighing: 40,
+  quantity: 5,
+  quantity_weighing: 10,
+  relations: 4,
+  relations_weighing: 10,
+  process_provider_id: process_provider1.id
+})
+
+Repo.insert!(%ProviderEvaluation{
+  credit: 4,
+  credit_weighing: 10,
+  punctuality: 4,
+  punctuality_weighing: 30,
+  quality: 3,
+  quality_weighing: 40,
+  quantity: 4,
+  quantity_weighing: 10,
+  relations: 5,
+  relations_weighing: 10,
+  process_provider_id: process_provider2.id
 })
 
 # Users
