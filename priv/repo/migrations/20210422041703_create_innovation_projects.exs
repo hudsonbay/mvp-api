aca02bc22f3c25287401370781c10265cf6c914d
hudsonbay <manuelmenendezalfonso@gmail.com>
2021-04-22 00:20:50 -0400

    Modified migrations

--- priv/repo/migrations/20210422041703_create_innovation_projects.exs
+++ priv/repo/migrations/20210422041703_create_innovation_projects.exs
@@ -7,7 +7,7 @@ defmodule MvpApi.Repo.Migrations.CreateInnovationProjects do
       add :name, :string, null: false
       add :objective, :string, null: false
       add :expected_result, :string, null: false
-      add :budget, :decimal, null: false
+      add :budget, :money_with_currency, null: false
       add :term, :decimal, null: false
       add :expected_anual_effect, :decimal, null: false
       add :observation, :text
