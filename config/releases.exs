079e83a34530d65d4fd222d29285855dd982e822
hudsonbay <manuelmenendezalfonso@gmail.com>
2021-09-11 03:44:11 -0400

    fix(releases): :bug: Fixed config isues for releases

--- config/releases.exs
+++ config/releases.exs
@@ -1,30 +1,24 @@
 import Config
 
 if config_env() == :prod do
-  secret_key_base =
-    System.get_env("SECRET_KEY_BASE") ||
-      raise """
-      environment variable SECRET_KEY_BASE is missing.
-      You can generate one by calling: mix phx.gen.secret
-      """
+  secret_key_base = "iy8zXCj/z2kOz9QAriICX5L0veOe/JpdRVeWJYAL3wBXBZK5aplrHNSqqk1mmJ8h"
 
-  app_name =
-    System.get_env("FLY_APP_NAME") ||
-      "mvp_api"
+  # app_name =
+  #   System.get_env("FLY_APP_NAME") ||
+  #     "mvp_api"
 
   config :mvp_api, MvpApiWeb.Endpoint,
     server: true,
-    url: [host: "#{app_name}.fly.dev", port: 80],
+    # url: [host: "#{app_name}.fly.dev", port: 80],
     http: [
       port: String.to_integer(System.get_env("PORT") || "4000"),
       # IMPORTANT: support IPv6 addresses
       transport_options: [socket_opts: [:inet6]]
     ],
-    secret_key_base: secret_key_base
+    secret_key_base: secret_key_base,
+    check_origin: false
 
-  database_url =
-    System.get_env("DATABASE_URL") ||
-      "ecto://postgres:postgres@localhost/mvp_api"
+  database_url = "ecto://postgres:postgres@localhost/mvp_api"
 
   config :mvp_api, MvpApi.Repo,
     url: database_url,
