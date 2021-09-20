2e202d6742c517df617acb4b5e93d7eeb4472ff2
hudsonbay <manuelmenendezalfonso@gmail.com>
2021-09-20 01:58:37 -0400

    fix: :fire: Fixed bug with config

--- config/prod.exs
+++ config/prod.exs
@@ -36,7 +36,7 @@ config :logger, level: :info
 config :mvp_api, MvpApi.Repo,
   adapter: Ecto.Adapters.Postgres,
   url: System.get_env("DATABASE_URL"),
-  pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10")
+  pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10"),
   ssl: true
 
 # ## SSL Support
